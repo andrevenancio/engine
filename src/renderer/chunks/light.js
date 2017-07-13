92005331b50b32fedf543ef471728bc11eb8176e
Andre Venancio <noreply@andrevenancio.com>
2017-07-13 10:13:41 +0100

    fix directional light

--- src/renderer/chunks/light.js
+++ src/renderer/chunks/light.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+// https://learnopengl.com/#!Lighting/Multiple-lights
 const DIRECTIONAL = {
     before: () => {
         return `
@@ -20,9 +21,11 @@ const DIRECTIONAL = {
             for (int i = 0; i < MAX_DIRECTIONAL; i++) {
                 vec3 inverseLightDirection = directionalLights[i].dlPosition.xyz * vec3(-1.0, -1.0, 1.0);
 
-                vec3 light = vec3(dot(v_normal, normalize(inverseLightDirection)));
+                vec3 light = vec3(max(dot(v_normal, normalize(inverseLightDirection)), 0.0));
                 vec3 directionalColor = directionalLights[i].dlColor.rgb * light;
                 dcolor += mix(dcolor, directionalColor, directionalLights[i].flIntensity);
+
+                // TODO implement specularity
             }
             dcolor /= float(MAX_DIRECTIONAL);
             base.rgb = dcolor;
